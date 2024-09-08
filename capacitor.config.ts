1eadc5fcb9d4987b72b0d47f319d44819f6760ae
GabPeron <jgabriel.s.peron@gmail.com>
2024-09-08 13:41:40 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'trabalho_pasta',
+  webDir: 'www'
+};
+
+export default config;
